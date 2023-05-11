f37d033426f4a2416a515c5e6914c800ac6f5b6b
Roshan Gyawali <roshangyawali9@gmail.com>
2023-05-11 12:17:50 +0545

    fix: form submittion

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,24 +1,32 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
-const YouTubeForm = () => {
-  const { register, control } = useForm();
 
+let renderCount = 0;
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
+const YouTubeForm = () => {
+  const { register, control, handleSubmit } = useForm<FormValues>();
+
+  const formSubmithandler = (data: FormValues) => {
+    console.log("form submitted", data);
+  };
   return (
     <div>
-      <h1>YouTube Form</h1>
-      <form>
+      <h1>YouTube Form ({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(formSubmithandler)}>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
-
         <label htmlFor="email">Email</label>
         <input type="email" id="email" {...register("email")} />
-
         <label htmlFor="channel"> Channel</label>
         <input type="text" id="channel" {...register("channel")} />
-
         <button>Submit</button>
       </form>
-      <DevTool  control={control}/>
+      <DevTool control={control} />
     </div>
   );
 };
