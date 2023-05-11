2c53ca48cdd859d7818707da4ac8c1c06339a44c
Roshan Gyawali <roshangyawali9@gmail.com>
2023-05-11 14:40:31 +0545

    fix: validation and display error message

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -9,7 +9,12 @@ type FormValues = {
 };
 
 const YouTubeForm = () => {
-  const { register, control, handleSubmit } = useForm<FormValues>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
   const formSubmithandler = (data: FormValues) => {
     console.log("form submitted", data);
@@ -18,12 +23,35 @@ const YouTubeForm = () => {
     <div>
       <h1>YouTube Form ({renderCount / 2})</h1>
       <form onSubmit={handleSubmit(formSubmithandler)}>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
-        <label htmlFor="channel"> Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "User name is required!",
+              },
+            })}
+          />
+          <p className="error">{errors?.username?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" {...register("email")} />
+          <p className="error">{errors?.email?.message}</p>
+        </div>
+        <div className="form-control">
+          <label htmlFor="channel"> Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", { required: "Channel is required!" })}
+          />
+          <p className="error">{errors?.channel?.message}</p>
+        </div>
+
         <button>Submit</button>
       </form>
       <DevTool control={control} />
