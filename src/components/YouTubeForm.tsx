6b98490bf87ceb36e2a6bd82969826e7bb6a23b5
Roshan Gyawali <roshangyawali9@gmail.com>
2023-05-11 11:09:01 +0545

    fix: form structure and styling

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const YouTubeForm = () => {
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="channel"> Channel</label>
+        <input type="text" id="channel" name="channel" />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YouTubeForm;
